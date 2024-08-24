458edf483c892253f88c372af3529ad016b05560
Yashrajsalunkhe <yashrajsalunkhe6@gmail.com>
2024-08-24 23:28:17 +0530

    Update site content

--- contact.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    // Prevent the default form submission
-    event.preventDefault();
-
-    // You can add validation or AJAX request here
-    alert('Thank you for your message! We will get back to you soon.');
-});
